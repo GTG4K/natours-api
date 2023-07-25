afc4f5f1fa22bce173436c4e3c21dd3d59bf0657
giorgi tarkhnishvili <giorgi.tarkhnishvili@redberry.ge>
2023-07-25 21:57:46 +0400

    feat: middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const tourRouter = express.Router();
 const tourController = require('../controllers/tourController');
+const { checkNameAndPrice } = require('../middleware/checkBody');
 
-tourRouter.route('/').get(tourController.getAllTours).post(tourController.addTour);
+tourRouter.param('id', (req, res, next, value) => {
+  console.log(`Tour ID is ${value} 😘`);
+  next();
+});
+
+tourRouter.route('/').get(tourController.getAllTours).post(checkNameAndPrice, tourController.addTour);
 tourRouter.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = tourRouter;
