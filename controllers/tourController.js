04c6a8b701e45c750560fa8468ec9bae3a8c7d8a
GTG4K <madtitan2002@gmail.com>
2023-07-28 11:26:41 -0700

    feat: add tour request with DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,14 +13,22 @@ exports.getTour = (req, res) => {
 
   // res.status(200).json({ status: 'success', data: { tour } });
 };
-exports.addTour = (req, res) => {
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newId }, req.body);
-  // tours.push(newTour);
+exports.addTour = async (req, res) => {
+  try{
+    const newTour = await Tour.create(req.body);
 
-  // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-  //   res.status(201).json({ status: 'success', data: { tour: newTour } });
-  // });
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour 
+      } 
+    });
+  }catch(err){
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   // const tourId = Number(req.params.id);
