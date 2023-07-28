f38dabf1ca372cfe4968c9d90f6e0c248294fe34
GTG4K <madtitan2002@gmail.com>
2023-07-28 11:30:42 -0700

    feat: get all tours request with DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,23 @@
 const Tour = require('../models/Tour');
 
-exports.getAllTours = (req, res) => {
-  // res.status(200).json({ status: 'success', results: tours.length, data: { tours } });
+exports.getAllTours = async (req, res) => {
+  try{
+    const tours = await Tour.find()
+
+    res.status(200).json(
+      { status: 'success', 
+      results: tours.length, 
+      data: { tours } 
+    });
+  }catch(err){
+    res.status(404).json(
+      { status: 'fail', 
+      message: err
+    });
+  }
+
 };
+
 exports.getTour = (req, res) => {
   // const tourId = Number(req.params.id);
   // const tour = tours.find(tour => tour.id === tourId);
@@ -16,7 +31,7 @@ exports.getTour = (req, res) => {
 exports.addTour = async (req, res) => {
   try{
     const newTour = await Tour.create(req.body);
-
+  
     res.status(201).json({
       status: 'success',
       data: {
