fb9de6a641dbd188eb40bded1cb91b2edce10bf0
GTG4K <madtitan2002@gmail.com>
2023-07-28 12:09:13 -0700

    fix: don't await value on delete

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -71,7 +71,7 @@ exports.updateTour = async (req, res) => {
 };
 exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndDelete(req.params.id)
+    await Tour.findByIdAndDelete(req.params.id)
 
     res.status(200).json({
       status: 'success',
